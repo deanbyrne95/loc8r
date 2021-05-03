27d2f4a4a1adc7880f9a44feb05e54346fa72a9d
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-03 19:31:59 +0100

    add pageContent data

--- app_public/src/app/components/homepage/homepage.component.ts
+++ app_public/src/app/components/homepage/homepage.component.ts
@@ -12,4 +12,12 @@ export class HomepageComponent implements OnInit {
   ngOnInit() {
   }
 
+  public pageContent = {
+    header: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with Wi-Fi near you!'
+    },
+    sidebar: 'Looking for Wi-Fi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.'
+  };
+
 }
