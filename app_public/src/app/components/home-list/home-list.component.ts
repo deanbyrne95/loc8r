b3266787266d2f3a5af1e8ca811df902f7c4d1b8
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-03 21:09:56 +0100

    add geolocation service

--- app_public/src/app/components/home-list/home-list.component.ts
+++ app_public/src/app/components/home-list/home-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from 'src/app/services/geolocation.service';
 import { Loc8rDataService } from '../../services/loc8r-data.service';
 
 export class Location {
@@ -17,17 +18,44 @@ export class Location {
 })
 export class HomeListComponent implements OnInit {
   public locations: Location[];
+  public message: string;
 
-  constructor(private loc8rDataService: Loc8rDataService) { }
+  constructor(
+    private loc8rDataService: Loc8rDataService,
+    private geolocationService: GeolocationService
+  ) { }
 
-  private getLocations(): void {
+  private getPosition(): void {
+    this.message = 'Getting your location...';
+    this.geolocationService.getPosition(
+      this.getLocations.bind(this),
+      this.showError.bind(this),
+      this.noGeo.bind(this)
+    );
+  }
+
+  private getLocations(position: any): void {
+    this.message = 'Searching for nearby places...';
+    const lat: number = position.coords.latitude;
+    const lng: number = position.coords.longitude;
     this.loc8rDataService
-      .getLocations()
-      .then(foundLocations => this.locations = foundLocations);
+      .getLocations(lat, lng)
+      .then(foundLocations => {
+        this.message = foundLocations.length > 0 ? '' : 'No locations found';
+        this.locations = foundLocations;
+      });
+  }
+
+  private showError(error: any): void {
+    this.message = error.message;
+  }
+
+  private noGeo(): void {
+    this.message = 'Geolocation not supported by this browser.'
   }
 
   ngOnInit() {
-    this.getLocations();
+    this.getPosition();
   }
 
 }
