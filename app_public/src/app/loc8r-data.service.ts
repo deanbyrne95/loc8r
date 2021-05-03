2cabb8e0da299da46bbe8e7239bf8607800fe22f
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-03 17:47:36 +0100

    add loc8r service to retrieve data

---
+++ app_public/src/app/loc8r-data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Location } from './home-list/home-list.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Loc8rDataService {
+
+  private baseApiUrl = 'http://localhost:3000/api'
+
+  constructor(private http: HttpClient) { }
+
+  public async getLocations(): Promise<Location[]> {
+    const lng: number = -0.7992599;
+    const lat: number = 51.378091;
+    const maxDistance: number = 20;
+    const url: string = `${this.baseApiUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
+    try {
+      const response = await this.http
+        .get(url)
+        .toPromise();
+      return response as Location[];
+    } catch (error) {
+      return this.handleError(error);
+    }
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Something has gone wrong: ', error);
+    return Promise.reject(error.message || error);
+  }
+}
