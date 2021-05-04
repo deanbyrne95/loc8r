7146f150e2ab459c8b584685f2f9b35fc72b22e2
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-04 14:45:15 +0100

    add location details info, add order pipe

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { HomeListComponent } from './components/home-list/home-list.component';
-import { DistancePipe } from './pipes/distance.pipe';
-import { RouterModule } from '@angular/router';
-import { FrameworkComponent } from './components/framework/framework.component';
-import { AboutComponent } from './components/about/about.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
-import { PageHeaderComponent } from './components/page-header/page-header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HtmlLineBreaksPipe } from './pipes/html-line-breaks.pipe';
-import { RatingStarsComponent } from './components/rating-stars/rating-stars.component';
-import { LocationDetailsComponent } from './components/location-details/location-details.component';
-import { DetailsPageComponent } from './components/details-page/details-page.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HomeListComponent } from "./components/home-list/home-list.component";
+import { DistancePipe } from "./pipes/distance.pipe";
+import { FrameworkComponent } from "./components/framework/framework.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HomepageComponent } from "./components/homepage/homepage.component";
+import { PageHeaderComponent } from "./components/page-header/page-header.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { HtmlLineBreaksPipe } from "./pipes/html-line-breaks.pipe";
+import { RatingStarsComponent } from "./components/rating-stars/rating-stars.component";
+import { LocationDetailsComponent } from "./components/location-details/location-details.component";
+import { DetailsPageComponent } from "./components/details-page/details-page.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { MostRecentOrderPipe } from "./pipes/most-recent-order.pipe";
 
 @NgModule({
   declarations: [
@@ -28,14 +28,11 @@ import { AppRoutingModule } from './app-routing.module';
     HtmlLineBreaksPipe,
     RatingStarsComponent,
     LocationDetailsComponent,
-    DetailsPageComponent
+    DetailsPageComponent,
+    MostRecentOrderPipe,
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [FrameworkComponent]
+  bootstrap: [FrameworkComponent],
 })
-export class AppModule { }
+export class AppModule {}
