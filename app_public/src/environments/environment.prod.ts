c42dbcc5d058d7dc0e918da74680e20ec3bf58ba
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-04 15:58:13 +0100

    update prod environment

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseApiUrl: "https://murmuring-brook-43687.herokuapp.com",
-  mapsApiKey: process.env.MAPS_API,
+  baseApiUrl: "https://murmuring-brook-43687.herokuapp.com/api",
+  mapsApiKey: "AIzaSyCAmPHGlJBD4Jg510N89xcEFe5OnFiWIFo",
   production: true
 };
