6d7c8dbc8ffd318ab82a941bfebbd4644e1eb745
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-04 19:49:14 +0100

    add authentication controller routes, format file

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,29 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const locationsController = require('../controllers/locations');
-const reviewsController = require('../controllers/reviews');
+const locationsController = require("../controllers/locations");
+const reviewsController = require("../controllers/reviews");
+const authenticationController = require("../controllers/authentication");
 
 // LOCATIONS
 router
-    .route('/locations')
-    .get(locationsController.listLocationsByDistance)
-    .post(locationsController.createLocation);
+  .route("/locations")
+  .get(locationsController.listLocationsByDistance)
+  .post(locationsController.createLocation);
 
 router
-    .route('/locations/:locationId')
-    .get(locationsController.getLocation)
-    .put(locationsController.updateLocation)
-    .delete(locationsController.deleteLocation);
+  .route("/locations/:locationId")
+  .get(locationsController.getLocation)
+  .put(locationsController.updateLocation)
+  .delete(locationsController.deleteLocation);
 
 // REVIEWS
 router
-    .route('/locations/:locationId/reviews')
-    .post(reviewsController.createReview);
+  .route("/locations/:locationId/reviews")
+  .post(reviewsController.createReview);
 
 router
-    .route('/locations/:locationId/reviews/:reviewId')
-    .get(reviewsController.getReview)
-    .put(reviewsController.updateReview)
-    .delete(reviewsController.deleteReview);
+  .route("/locations/:locationId/reviews/:reviewId")
+  .get(reviewsController.getReview)
+  .put(reviewsController.updateReview)
+  .delete(reviewsController.deleteReview);
+
+router.post("/register", authenticationController.register);
+router.post("/login", authenticationController.login);
 
 module.exports = router;
