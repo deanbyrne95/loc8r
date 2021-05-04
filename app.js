4522624640753bd368e28ccd71bed9e771293c99
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-04 20:51:42 +0100

    add unauthorised error handler

--- app.js
+++ app.js
@@ -55,6 +55,14 @@ app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function (req, res, next) {
   );
 });
 
+// error handlers
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ 'message': err.name + ": " + err.message });
+  }
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
