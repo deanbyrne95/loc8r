37aa05b6d9fc52300164d2cf734cd116777481da
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-04 14:44:11 +0100

    update details page to include locations by ID

--- app_public/src/app/components/details-page/details-page.component.ts
+++ app_public/src/app/components/details-page/details-page.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { switchMap } from "rxjs/operators";
+import { Loc8rDataService } from "src/app/services/loc8r-data.service";
+import { Location } from "../home-list/home-list.component";
 
 @Component({
-  selector: 'app-details-page',
-  templateUrl: './details-page.component.html',
-  styleUrls: ['./details-page.component.css']
+  selector: "app-details-page",
+  templateUrl: "./details-page.component.html",
+  styleUrls: ["./details-page.component.css"],
 })
 export class DetailsPageComponent implements OnInit {
+  location: Location;
 
-  constructor() { }
+  constructor(
+    private loc8rDataService: Loc8rDataService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) => {
+          let id = params.get("locationId");
+          return this.loc8rDataService.getLocationById(id);
+        })
+      )
+      .subscribe((location: Location) => {
+        this.location = location;
+        this.pageContent.header.title = location.name;
+        this.pageContent.sidebar = `${location.name} is on Loc8r because it has accessible Wi-Fi and space to sit down with your laptop and get some work done.\n\nIf you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.`;
+      });
   }
 
   public pageContent = {
     header: {
-      title: 'Location name',
-      strapline: ''
+      title: "",
+      strapline: "",
     },
-    sidebar: 'is on Loc8r because it has accessible Wi-Fi and space to sit down with your laptop and get some work done.\n\nIf you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-  }
-
+    sidebar: "",
+  };
 }
--- app_public/src/app/services/loc8r-data.service.ts
+++ app_public/src/app/services/loc8r-data.service.ts
@@ -1,28 +1,36 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Location } from '../components/home-list/home-list.component';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Location } from "../components/home-list/home-list.component";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class Loc8rDataService {
+  private baseApiUrl = "http://localhost:3000/api";
 
-  private baseApiUrl = 'http://localhost:3000/api'
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public getLocations(lat: number, lng: number): Promise<Location[]> {
     const maxDistance: number = 2000000;
     const url: string = `${this.baseApiUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
-      return this.http
-        .get(url)
-        .toPromise()
-        .then(response => response as Location[])
-        .catch(this.handleError);
+    return this.http
+      .get(url)
+      .toPromise()
+      .then((response) => response as Location[])
+      .catch(this.handleError);
+  }
+
+  public getLocationById(locationId: string): Promise<Location> {
+    const url: string = `${this.baseApiUrl}/locations/${locationId}`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then((response) => response as Location)
+      .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
-    console.error('Something has gone wrong: ', error);
+    console.error("Something has gone wrong: ", error);
     return Promise.reject(error.message || error);
   }
 }
