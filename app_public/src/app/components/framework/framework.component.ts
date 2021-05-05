b13caf31b3466a6af2f7cca26d8c98fbf0232693
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-05 15:06:44 +0100

    add history service to framework to track page history

--- app_public/src/app/components/framework/framework.component.ts
+++ app_public/src/app/components/framework/framework.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { User } from "src/app/classes/user";
 import { AuthenticationService } from "src/app/services/authentication.service";
+import { HistoryService } from "src/app/services/history.service";
 
 @Component({
   selector: "app-framework",
@@ -8,7 +9,10 @@ import { AuthenticationService } from "src/app/services/authentication.service";
   styleUrls: ["./framework.component.css"],
 })
 export class FrameworkComponent implements OnInit {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private historyService: HistoryService
+  ) {}
 
   ngOnInit() {}
 
