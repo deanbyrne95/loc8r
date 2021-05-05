7524ae6c0d15ccdb6a26386276810232a6b0638e
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-05 14:49:55 +0100

    add sign-in, logout capabilities from navbar

--- app_public/src/app/components/framework/framework.component.ts
+++ app_public/src/app/components/framework/framework.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "src/app/classes/user";
+import { AuthenticationService } from "src/app/services/authentication.service";
 
 @Component({
-  selector: 'app-framework',
-  templateUrl: './framework.component.html',
-  styleUrls: ['./framework.component.css']
+  selector: "app-framework",
+  templateUrl: "./framework.component.html",
+  styleUrls: ["./framework.component.css"],
 })
 export class FrameworkComponent implements OnInit {
+  constructor(private authenticationService: AuthenticationService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public logout(): void {
+    this.authenticationService.logout();
   }
 
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+
+  public getUsername(): string {
+    const user: User = this.authenticationService.getCurrentUser();
+    return user ? user.name : "Guest";
+  }
 }
