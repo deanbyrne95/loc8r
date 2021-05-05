5b36f64a0016b18c2fc236485068b24bf3d61226
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-05 09:52:58 +0100

    add login/register methods, add logout, call on service

--- app_public/src/app/services/authentication.service.ts
+++ app_public/src/app/services/authentication.service.ts
@@ -1,11 +1,17 @@
 import { Inject, Injectable } from "@angular/core";
+import { AuthResponse } from "../classes/authresponse";
 import { BROWSER_STORAGE } from "../classes/storage";
+import { User } from "../classes/user";
+import { Loc8rDataService } from "./loc8r-data.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthenticationService {
-  constructor(@Inject(BROWSER_STORAGE) private storage: Storage) {}
+  constructor(
+    @Inject(BROWSER_STORAGE) private storage: Storage,
+    private loc8rDataService: Loc8rDataService
+  ) {}
 
   public getToken(): string {
     return this.storage.getItem("loc8r-token");
@@ -13,5 +19,21 @@ export class AuthenticationService {
 
   public saveToken(token: string): void {
     this.storage.setItem("loc8r-token", token);
+  }
+
+  public login(user: User): Promise<any> {
+    return this.loc8rDataService
+      .login(user)
+      .then((authResponse: AuthResponse) => this.saveToken(authResponse.token));
+  }
+
+  public register(user: User): Promise<any> {
+    return this.loc8rDataService
+      .register(user)
+      .then((authResponse: AuthResponse) => this.saveToken(authResponse.token));
+  }
+
+  public logout(): void {
+    this.storage.removeItem("loc8r-token");
   }
 }
