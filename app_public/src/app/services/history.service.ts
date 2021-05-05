52134f23615e1f4fa675285b5d06bb8ebd579e6a
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-05 14:56:00 +0100

    create history service to allow redirection to previous pages

---
+++ app_public/src/app/services/history.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HistoryService {
+  private urls: string[] = [];
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((routerEvent) => routerEvent instanceof NavigationEnd))
+      .subscribe((routerEvent: NavigationEnd) => {
+        const url = routerEvent.urlAfterRedirects;
+        this.urls = [...this.urls, url];
+      });
+  }
+
+  public getPreviousUrl(): string {
+    const length = this.urls.length;
+    return length > 1 ? this.urls[length - 2] : "/";
+  }
+}
