06e009955735d499c56c6d4b39b8838bca5a7eeb
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-05 15:39:01 +0100

    fix authentication algorithm

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,7 +4,7 @@ const jwt = require('express-jwt');
 const auth = jwt({
     secret: process.env.JWT_SECRET,
     userProperty: 'payload',
-    algorithms: ['RS256'],
+    algorithms: ['HS256'],
 });
 const locationsController = require('../controllers/locations');
 const reviewsController = require('../controllers/reviews');
