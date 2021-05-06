d8a4541a98be50a6feebaa53d6e2c69c76ff76e1
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-05-06 15:45:40 +0100

    get authentication to return id

--- app_public/src/app/services/authentication.service.ts
+++ app_public/src/app/services/authentication.service.ts
@@ -24,8 +24,8 @@ export class AuthenticationService {
   public getCurrentUser(): User {
     if (this.isLoggedIn()) {
       const token: string = this.getToken();
-      const { email, name } = JSON.parse(atob(token.split(".")[1]));
-      return { email, name } as User;
+      const { _id, email, name } = JSON.parse(atob(token.split(".")[1]));
+      return {_id,  email, name } as User;
     }
   }
 
